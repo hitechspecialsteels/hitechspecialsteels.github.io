c1fc0b66b5f412495b29bf20533da5fb5b00efe2
hitechspecialsteels <36447135+hitechspecialsteels@users.noreply.github.com>
2018-04-09 00:04:19 +0100

    Initial website source commit.

---
+++ scripts/script_main.js
@@ -0,0 +1,41 @@
+// navigation bar - responsive navbar open
+function navopenFunction() {
+    var x = document.getElementById("pagenavbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+
+    var open = document.getElementById("opennavicon");
+    var close = document.getElementById("closenavicon");
+    if (open.className === "activeicon") {
+      close.className = "activeicon";
+      open.className = "";
+    } else {
+      close.className = "";
+      open.className = "activeicon";
+    }
+}
+
+// navigation bar - sticky navbar
+var hasScrolled = false;
+var topnavbar;
+var navbaroffset;
+var firstpageelement;
+function stickynavFunction() {
+  if (hasScrolled) {
+    if (window.pageYOffset >= navbaroffset) {
+      topnavbar.classList.add("sticky");
+      firstpageelement.classList.add("navpadding");
+    } else {
+      topnavbar.classList.remove("sticky");
+      firstpageelement.classList.remove("navpadding");
+    }
+  } else {
+    topnavbar = document.getElementById("nav");
+    navbaroffset = topnavbar.offsetTop;
+    firstpageelement = document.getElementById("firstelement");
+    hasScrolled = true;
+  }
+}
